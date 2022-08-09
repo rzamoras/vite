34776b0daf8f233200e4935e5dbf481acc1a01db
xxx <rayzamoras159@gmail.com>
2022-08-10 00:18:30 +0800

    routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "./../views/Home.vue";
+import About from "./../views/About.vue";
 
-const routes = [{
+const routes = [
+    {
     path: "/",
     name: "home",
     component: Home
-}]
+    },
+    {
+    path: "/about",
+    name: "about",
+    component: About
+    }
+]
 
 const router = createRouter({
     history: createWebHistory(),
