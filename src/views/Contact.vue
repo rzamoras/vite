039a4eaa5e2dfc502094c5c1c8f214ce348de2c8
xxx <rayzamoras159@gmail.com>
2022-08-13 13:57:59 +0800

    biringan city

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -25,7 +25,7 @@
 						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 mr-2 sm:mr-6">
 							<path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd"></path>
 						</svg>
-						<span>123 Street, Biringan, Biringan City</span>
+						<span>123 Street, Biringan City</span>
 					</p>
 					<p class="flex items-center">
 						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 mr-2 sm:mr-6">
